1e9e2b46f3e32280d5da74ee065b7796d9c3bb7a
Stephen Fairhurst <stephen.fairhurst@ligo.org>
2016-11-21 15:24:12 +0000

    clean up code, calculate snr at offset points

--- localize.py
+++ localize.py
@@ -58,7 +58,7 @@ def evec_sigma(M):
 ##################################################################
 # Class to store detector information
 ##################################################################
-class det(object):
+class Det(object):
     """
     class to hold the details of a detector
     """
@@ -131,7 +131,7 @@ class det(object):
 ##################################################################
 # Class to store network information 
 ##################################################################
-class network(object):
+class Network(object):
     """
     class to hold the details of the network.
     """
@@ -157,7 +157,7 @@ class network(object):
         :param loc_thresh: threshold for declaring an event localized
         :param duty_cycle: fraction of time the detector is operational
         """
-        d = det(location, response, det_range, f_mean, f_band,
+        d = Det(location, response, det_range, f_mean, f_band,
                 found_thresh, loc_thresh, duty_cycle)
         setattr(self, ifo, d)
         self.ifos.append(ifo)
@@ -189,103 +189,49 @@ class network(object):
         """
         return array([getattr(getattr(self, i), data) for i in self.ifos])
 
-
-##################################################################
-# Class to store localization information
-##################################################################
-class localization(object):
-    """
-    class to hold the details of a localization method
-    """
-
-    def __init__(self, method, M, snr, dt, z, event, mirror=False,
-                 p=0.9, Dmax=1000, area = 0):
-        """
-        Initialization
-
-        :param method: how we do localization, one of "time", "coh", "left, "right", "marg"
-        :param M: localization matrix
-        :param snr: snr of event
-        :param dt: time offset
-        :param z: complex snr
-        :param event: details of event
-        :param mirror: are we looking in the mirror location
-        :param p: probability
-        :param Dmax: maximum distance to consider
-        """
-        self.method = method
-        self.M = M
-        self.snr = snr
-        self.dt = dt
-        self.z = z
-        self.mirror = mirror
-        self.p = p
-        self.like = 0.
-        A  = fstat.snr_f_to_a(self.z, event.get_fsig(mirror))
-        self.D, self.cosi, _, _ = fstat.a_to_params(A)
-        if method is not "time" and method is not "marg": 
-            self.approx_like(event, Dmax)
-        
-        if area != 0:
-            self.area = area
-        else:
-            if M is not None:
-                self.sky_project(event)
-                self.calc_area()
-            else:
-                self.area = 1e6
-
-    def sky_project(self, event):
-        """
-        Project localization matrix to zero out components in direction of source
-        This is implementing equations 10 and 11 from the advanced localization paper
-        :param event: structure with the event details
-        """
-        if self.mirror == True:
-            source = event.mirror_xyz
-        else:
-            source = event.xyz
-        P = identity(3) - outer(source, source)
-        self.PMP = inner(inner(P, self.M), P)
-        self.evec, self.sigma = evec_sigma(self.PMP)
-
-    def calc_area(self):
-        """
-        Calculate the localization area
-        :param p: probability for event to be contained in the given area
-        """
-        # calculate the area of the ellipse
-        ellipse = - log(1. - self.p) * 2 * math.pi * (180 / math.pi) ** 2 * \
-                self.sigma[0] * self.sigma[1]
-        # calculate the area of the band 
-        band = 4 * math.pi * (180 / math.pi) ** 2 * sqrt(2) * special.erfinv(self.p) * self.sigma[0]
-        # use the minimum (that's not nan)
-        self.area = nanmin((ellipse, band))
-
-    def approx_like(self, event, Dmax=1000):
-        """
-        Calculate the approximate likelihood, based on equations XXX
-        :param event: structure giving details of the event
-        :param mirror: boolean indicating whether we are looking at the mirror position
-        :param Dmax: maximum distance, used for normalization
-        """
-        if self.snr == 0:
-            self.like = 0
-            return
-        Fp, Fc = event.get_f(self.mirror)
-        self.like = self.snr ** 2 / 2
-        if (self.method == "left") or (self.method == "right"):
-            cos_fac = sqrt((Fp ** 2 + Fc ** 2) / (Fp * Fc))
-            cosf = min(cos_fac / sqrt(self.snr), 0.5)
-            self.like += log((self.D / Dmax) ** 3 / self.snr ** 2 * cosf)
-        else:
-            self.like += log(32. * (self.D / Dmax) ** 3 * self.D ** 4 / (Fp ** 2 * Fc ** 2)
-                             / (1 - self.cosi ** 2) ** 3)
+################################
+# plot the localization detector ellipse.  
+# Note that this assumes the ellipse is centred on the event 
+################################
+def projection_ellipse(loc, event):
+  # set up co-ordinates
+  r = {}
+  dangle=  pi/50.0
+  angle= arange(0,2*pi+dangle*0.5,dangle)
+  # calculate the basis
+  x_net = loc.evec[:,0]
+  y_net = loc.evec[:,1]
+  z_net = loc.evec[:,2]
+  # normalization to get 90% area:
+  source = event.xyz
+  x = inner(source,x_net) + \
+      sqrt(2 * log(10)) *  loc.sigma[0] * cos(angle) 
+  y = inner(source,y_net) + \
+      sqrt(2 * log(10)) *  loc.sigma[1] * sin(angle) 
+  z = sqrt(1 - x**2 - y**2) * sign(inner(source,z_net))
+  # check that we're not going outside of unit circle:
+  bad = x**2 + y**2 > 1
+  if sum(bad) > 0:
+    x = concatenate((x[bad.argmax():], x[0:bad.argmax()]))
+    y = concatenate((y[bad.argmax():], y[0:bad.argmax()]))
+    z = concatenate((z[bad.argmax():], z[0:bad.argmax()]))
+    bad = concatenate((bad[bad.argmax():], bad[0:bad.argmax()]))
+    x = x[~bad]
+    y = y[~bad]
+    z = z[~bad]
+    x = append(concatenate((x, x[::-1])), x[0])
+    y = append(concatenate((y, y[::-1])), y[0])
+    z = append(concatenate((z, -z[::-1])),z[0])
+  for i in xrange(3):
+    r[i] = x * x_net[i] + y * y_net[i] + z * z_net[i]
+  theta = arcsin(r[2] / sqrt(r[0]**2 + r[1]**2 + r[2]**2) )
+  phi = arctan2(r[1],r[0])
+  return phi, theta
 
 ##################################################################
 # Class to store event information
 ##################################################################
-class event(object):
+class Event(object):
     """
     class to hold the events that we want to localize
     """
@@ -386,11 +332,28 @@ class event(object):
         F.sort()
         return F[::-1]
 
-    def get_snr(self):
+    def get_snr(self, dt_i=None):
         """
         get the relevant data for each detector and return it as an array
+        if dt_i is given then shift the time in each detector by that amount
         """
-        return array([getattr(self, i).snr for i in self.ifos])
+        z = array([getattr(self, i).snr for i in self.ifos])
+        if dt_i is not None:
+            f_mean = self.get_data("f_mean")
+            f_band = self.get_data("f_band")
+            z *= (1 - 2 * pi ** 2 * (f_mean ** 2 + f_band ** 2) * dt_i ** 2
+                            + 2.j * pi * dt_i * f_mean)
+        return z
+
+    def projected_snr(self, method, mirror=False, dt_i = None):
+        """
+        Calculate the projected SNR for a given method at either original or mirror
+        sky location
+        """
+        f_sig = self.get_fsig(mirror)
+        P = snr_projection(f_sig, method)
+        zp = inner(self.get_snr(dt_i), P)
+        return (zp)
 
     def calculate_mirror(self):
         """
@@ -420,21 +383,10 @@ class event(object):
         if self.mirror:
             self.mirror_sensitivity = linalg.norm(self.get_fsig(mirror=True))
 
-
-    def projected_snr(self, method, mirror=False):
+    def localization_factors(self, method, mirror=False):
         """
-        Calculate the projected SNR for a given method at either original or mirror
-        sky location
-        """
-        f_sig = self.get_fsig(mirror)
-        P = snr_projection(f_sig, method)
-        zp = inner(self.get_snr(), P)
-        return (zp)
-
-
-    def localize(self, method, mirror=False, p=0.9):
-        """
-        Localization of a source by a network of detectors, given the
+        Calculate all of the localization factors for a given source
+        and network of detectors, given the
         complex snr, sensitivity, bandwidth, mean frequency, location
         of the detectors.
         Here, we keep all the projection operators -- required if Z is not
@@ -459,46 +411,23 @@ class event(object):
         c_i = sum(C_ij, axis=1)
         c = sum(c_i)
 
-        CC = 1. / 2 * (outer(c_i, c_i) / c - C_ij)
-        M = zeros([3, 3])
-
-        for i1 in xrange(len(self.ifos)):
-            for i2 in xrange(len(self.ifos)):
-                M += outer(locations[i1] - locations[i2],
-                           locations[i1] - locations[i2]) / (3e8) ** 2 \
-                     * CC[i1, i2]
-
-        # work out the offset factors
         A_i = 4 * pi * imag(sum(outer(f_mean * z.conjugate(), z) * P, axis=1))
-        try:
-            dt_i = 1. / 2 * inner(linalg.inv(C_ij), A_i)
-        except:
-            print("for method %s: Unable to invert C, setting dt=0" % method)
-            dt_i = zeros_like(A_i)
-        zp = self.projected_snr(method, mirror)
-        snrsq = linalg.norm(zp) ** 2
-        if max(abs(dt_i * (2 * pi * f_band))) < 1. / sqrt(2):
-            extra_snr = inner(dt_i, inner(C_ij, dt_i))
-            if extra_snr > 0:
-                # location of second peak is reasonable -- use it
-                snrsq += extra_snr
-                z_dt = z * (1 - 2 * pi ** 2 * (f_mean ** 2 + f_band ** 2) * dt_i ** 2
-                            + 2.j * pi * dt_i * f_mean)
-                zp = inner(z_dt, P)
-        else:
-            # failed to find a maximum within the linear regime
-            M = None 
-            snrsq = 0 
-            dt_i = 0
-            zp = zeros_like(zp)
+
+        return A_i, C_ij, c_i, c
 
 
+    def localize(self, method, mirror=False, p=0.9):
+        """
+        Localization of a source by a network of detectors, given the
+        complex snr, sensitivity, bandwidth, mean frequency, location
+        of the detectors.
+        Here, we keep all the projection operators -- required if Z is not
+        compatible with being a signal from the given sky location
+        """
         if mirror:
-            self.mirror_loc[method] = \
-                localization(method, M, sqrt(snrsq), dt_i, zp, self, mirror, p)
+            self.mirror_loc[method] = Localization(method, self, mirror, p)
         else:
-            self.localization[method] = \
-                localization(method, M, sqrt(snrsq), dt_i, zp, self, mirror, p)
+            self.localization[method] = Localization(method, self, mirror, p)
 
     def combined_loc(self, method):
         """
@@ -551,9 +480,7 @@ class event(object):
         r_max = 1.1 * sqrt(max([l[k].area for k in keys])/pi)
         r_min = 0.9 * sqrt(min([l[k].area for k in keys])/pi)
         r = brentq(f, r_min, r_max, (keys, l, p))
-        l["marg"] = localization("marg", M=None, snr=l["coh"].snr, dt=l["coh"].dt, 
-                                 z=l["coh"].z, event=self, mirror=mirror, 
-                                 p=p, area = pi*r**2)
+        l["marg"] = Localization("marg", self, mirror, p, area = pi*r**2)
         l["marg"].like = logsumexp([l[k].like + log(l[k].area) - log(-2*pi*log(1-l[k].p)) 
                                     for k in keys])
         l["marg"].like -= log(l["marg"].area) - log(-2*pi*log(1-p))
@@ -581,3 +508,147 @@ def f(r, keys, l, p):
         s2 = l[k].area/(-2*pi*log(1-l[k].p))
         f += exp(l[k].like - lmax) * s2 * (1 - p - exp(-r**2/(2*s2)))
     return f
+
+
+
+##################################################################
+# Class to store localization information
+##################################################################
+class Localization(object):
+    """
+    class to hold the details of a localization method
+    """
+
+    def __init__(self, method, event, mirror=False, p=0.9, Dmax=1000, area = 0):
+        """
+        Initialization
+
+        :param method: how we do localization, one of "time", "coh", "left, "right", "marg"
+        :param event: details of event
+        :param mirror: are we looking in the mirror location
+        :param p: probability
+        :param Dmax: maximum distance to consider
+        """
+        self.method = method
+        self.mirror = mirror
+        self.event = event
+        self.p = p
+        self.snr = 0
+        self.area = area
+        if method is not "marg":
+            self.calculate_m()
+            self.calculate_max_snr()
+            if self.M is not None:
+                self.sky_project()
+                self.calc_area()
+            else:
+                self.area = 1e6
+
+            A  = fstat.snr_f_to_a(self.z, self.event.get_fsig(mirror))
+            self.D, self.cosi, _, _ = fstat.a_to_params(A)
+
+        self.like = 0.
+        if method is not "time" and method is not "marg": 
+            self.approx_like(Dmax)
+
+    def calculate_m(self):
+        A_i, C_ij, c_i, c = self.event.localization_factors(self.method, self.mirror)
+        CC = 1. / 2 * (outer(c_i, c_i) / c - C_ij)
+        # Calculate the Matrix M (given in the coherent localization paper)
+        M = zeros([3, 3])
+        locations = self.event.get_data("location")
+
+        for i1 in xrange(len(self.event.ifos)):
+            for i2 in xrange(len(self.event.ifos)):
+                M += outer(locations[i1] - locations[i2],
+                    locations[i1] - locations[i2]) / (3e8) ** 2 \
+                    * CC[i1, i2]
+        self.M = M
+
+    def calculate_max_snr(self):
+        self.z = self.event.projected_snr(self.method, self.mirror)
+        A_i, C_ij, c_i, c = self.event.localization_factors(self.method, self.mirror)
+        try:
+            self.dt_i = 1. / 2 * inner(linalg.inv(C_ij), A_i)
+        except:
+            print("for method %s: Unable to invert C, setting dt=0" % self.method)
+            self.dt_i = zeros_like(A_i)
+        z = self.event.projected_snr(self.method, self.mirror)
+        f_band = self.event.get_data("f_band")
+        if max(abs(self.dt_i * (2 * pi * f_band))) < 1. / sqrt(2):
+            extra_snr = inner(self.dt_i, inner(C_ij, self.dt_i))
+            if extra_snr > 0:
+                # location of second peak is reasonable -- use it
+                z = self.event.projected_snr(self.method, self.mirror, self.dt_i)
+        self.z = z
+        self.snr = linalg.norm(z)
+
+    def calculate_dt0(self, dt_i):
+        """
+        Calculate the SNR at a point offset from the source point
+        dt_i is an array of time offsets for the detectors
+        Note: we maximize over the overall time offset dt_o
+        """
+        A_i, C_ij, c_i, c = self.event.localization_factors(self.method, self.mirror)
+        a = sum(A_i)
+        dt0 = a/(2*c) - inner(c_i, dt_i) / c
+        return dt0
+
+    def calculate_snr(self, dt):
+        A_i, C_ij, c_i, c = self.event.localization_factors(self.method, self.mirror)
+
+        # See if the calculation is valid:
+        f_band = self.event.get_data("f_band")
+        if max(abs(dt * (2 * pi * f_band))) > 1. / sqrt(2):
+            # location of second peak is outside linear regime, return zero
+            z = zeros_like(dt)
+        else:    
+            z = self.event.projected_snr(self.method, self.mirror, dt)
+        snr = linalg.norm(z)
+        return z, snr
+
+    def sky_project(self):
+        """
+        Project localization matrix to zero out components in direction of source
+        This is implementing equations 10 and 11 from the advanced localization paper
+        """
+        if self.mirror == True:
+            source = self.event.mirror_xyz
+        else:
+            source = self.event.xyz
+        P = identity(3) - outer(source, source)
+        self.PMP = inner(inner(P, self.M), P)
+        self.evec, self.sigma = evec_sigma(self.PMP)
+
+    def calc_area(self):
+        """
+        Calculate the localization area
+        :param p: probability for event to be contained in the given area
+        """
+        # calculate the area of the ellipse
+        ellipse = - log(1. - self.p) * 2 * math.pi * (180 / math.pi) ** 2 * \
+                self.sigma[0] * self.sigma[1]
+        # calculate the area of the band 
+        band = 4 * math.pi * (180 / math.pi) ** 2 * sqrt(2) * special.erfinv(self.p) * self.sigma[0]
+        # use the minimum (that's not nan)
+        self.area = nanmin((ellipse, band))
+
+    def approx_like(self, Dmax=1000):
+        """
+        Calculate the approximate likelihood, based on equations XXX
+        :param Dmax: maximum distance, used for normalization
+        """
+        if self.snr == 0:
+            self.like = 0
+            return
+        Fp, Fc = self.event.get_f(self.mirror)
+        self.like = self.snr ** 2 / 2
+        if (self.method == "left") or (self.method == "right"):
+            cos_fac = sqrt((Fp ** 2 + Fc ** 2) / (Fp * Fc))
+            cosf = min(cos_fac / sqrt(self.snr), 0.5)
+            self.like += log((self.D / Dmax) ** 3 / self.snr ** 2 * cosf)
+        else:
+            self.like += log(32. * (self.D / Dmax) ** 3 * self.D ** 4 / (Fp ** 2 * Fc ** 2)
+                             / (1 - self.cosi ** 2) ** 3)
+
+
