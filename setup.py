ec2b2fa3ef791d3892c22d8aceb6bcdea0ad6f04
Stephen Fairhurst <fairhursts@cardiff.ac.uk>
2020-12-14 12:57:11 +0000

    fix typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="simpe-pe"
+    name="simple-pe",
     version="0.0.1",
     author="Stephen Fairhurst",
     author_email="stephen.fairhurst@ligo.org",
