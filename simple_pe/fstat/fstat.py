23659f52a229d850a9d7001197e961ae0841423c
Stephen Fairhurst <stephen.fairhurst@ligo.org>
2022-01-12 18:04:55 +0000

    add log_like function, fix documentation

--- simple_pe/fstat/fstat.py
+++ simple_pe/fstat/fstat.py
@@ -1,4 +1,3 @@
-
 from numpy import *
 
 
@@ -10,11 +9,13 @@ def params_to_a(d, cosi, psi, phi=0, d0=1.):
     """
     Calculate the F-stat A params given the physical parameters and a choice of
     d0 to set the overall scaling
+    
     :param d: distance to source
     :param cosi: cos(inclination) of source
     :param psi: polarization of source
     :param phi: coalescence phase of source
     :param d0: overall scaling of A's
+    :returns: the f-stat params, with a[0] storing d0
     """
     a_plus = d0 / d * (1. + cosi ** 2) / 2
     a_cross = d0 / d * cosi
@@ -30,7 +31,9 @@ def params_to_a(d, cosi, psi, phi=0, d0=1.):
 def a_to_params(a):
     """
     Calculate the physical parameters based upon the F-stat A parameters.
+
     :param a: array of f-stat params, entry zero assumed to be d0
+    :returns: distance, cos(inclination), polarization, phase
     """
     # these variables are what they say [ (a_plus +/- a_cross)^2 ]
     ap_plus_ac_2 = (a[1] + a[4]) ** 2 + (a[2] - a[3]) ** 2
@@ -48,7 +51,9 @@ def a_to_params(a):
 def a_to_circular(a):
     """
     Calculate the circular F-stat A parameters given in Whelan et al 2013
+
     :param a: array of f-stat params, entry zero assumed to be d0
+    :returns: circular f-stat parameters
     """
     a_circ = zeros(5)
     a_circ[0] = a[0]
@@ -63,7 +68,9 @@ def a_to_circ_amp(a):
     """
     Calculate the amplitudes of left/right circularly polarized waveforms
     from the F-stat A parameters
+
     :param a: array of f-stat params, entry zero assumed to be d0
+    :returns: the amplitudes of the left and right polarizations
     """
     a_circ = a_to_circular(a)
     ar_hat = sqrt(a_circ[1] ** 2 + a_circ[2] ** 2)
@@ -74,7 +81,9 @@ def a_to_circ_amp(a):
 def phase_diff(a):
     """
     Calculate the phase difference between the plus and cross polarizations
+
     :param a: array of f-stat params, entry zero (unused) assumed to be d0
+    :returns: phase difference between the two polarizations
     """
     return arctan2(a[1] * a[4] - a[2] * a[3], a[1] * a[2] + a[3] * a[4])
 
@@ -82,7 +91,9 @@ def phase_diff(a):
 def amp_ratio(a):
     """
     Calculate the amplitude ratio
+
     :param a: array of f-stat params, entry zero assumed to be d0
+    :returns: the ratio of amplitudes of the two polarizations
     """
     return sqrt((a[1] ** 2 + a[3] ** 2) / (a[2] ** 2 + a[4] ** 2))
 
@@ -94,9 +105,11 @@ def amp_ratio(a):
 def expected_snr(a, f_plus, f_cross):
     """
     Calculate the SNR for a given set of A parameters and network sensitivity.
+
     :param a: the F-stat A parameters
     :param f_plus: F_plus sensitivity
     :param f_cross: F_cross sensitivity
+    :returns: the expected SNR
     """
     f = array([0, f_plus, f_cross, f_plus, f_cross])
     snrsq = sum(f ** 2 * a ** 2)
@@ -106,10 +119,12 @@ def expected_snr(a, f_plus, f_cross):
 def set_snr(a, f_plus, f_cross, snr):
     """
     rescale distance to give desired SNR, return rescaled as and distance
+
     :param a: the F-stat A parameters
     :param f_plus: F_plus sensitivity
     :param f_cross: F_cross sensitivity
     :param snr: the desired SNR
+    :returns: scaled f-stat parameters to a specific SNR
     """
     s = expected_snr(a, f_plus, f_cross)
     a_scale = a * snr / s
@@ -122,23 +137,45 @@ def lost_snrsq(a_hat, a, f_plus, f_cross):
     """
     Calculate the difference in SNRSQ between the true parameters a_hat
     and the template a, network sensitivity f_plus, f_cross
+
     :param a_hat: the observed F-stat A parameters
     :param a: the "template" F-stat A parameters
     :param f_plus: sensitivity to plus polarization
     :param f_cross: sensitivity to cross polarization
+    :returns: loss in SNR from incorrect f-stat parameters
     """
     f = array([0, f_plus, f_cross, f_plus, f_cross])
     snrsq = sum(f ** 2 * (a_hat - a) ** 2)
     return snrsq
 
 
+def log_like(a_hat, f_plus, f_cross, d, cosi, psi, phi, d0=1):
+    """
+    Calculate the log likelihood of at the given physical parameters
+    for a signal described by a_hat and a network sensitivity f_plus, f_cross
+
+    :param a_hat: the observed F-stat A parameters
+    :param f_plus: sensitivity to plus polarization
+    :param f_cross: sensitivity to cross polarization
+    :param d: distance to source
+    :param cosi: cos(inclination) of source
+    :param psi: polarization of source
+    :param phi: coalescence phase of source
+    :param d0: overall scaling of A's
+    :returns: the log likelihood (relative to the peak) at the given point
+    """
+    return -0.5 * lost_snrsq(a_hat, params_to_a(d, cosi, psi, phi, d0), f_plus, f_cross)
+
+
 def circ_project(a, f_plus, f_cross, hand):
     """
     Project the f-stat A parameters to those that would be observed by restricting
     to left or right circular polarization
+
     :param f_plus: sensitivity to plus polarization
     :param f_cross: sensitivity to cross polarization
     :param hand: one of "left", "right"
+    :returns: f-stat A parameters projected to left or right circularly polarized system
     """
     if hand == "right":
         x = 1
@@ -163,13 +200,16 @@ def circ_project(a, f_plus, f_cross, hand):
     a_proj[isnan(a_proj)] = 0
     return a_proj
 
+
 # These functions allow us to go from SNRs to F-stat params and back
 
 def snr_f_to_a(z, f_sig):
     """
     Given the complex SNR and the detector sensitivities, calculate the f-stat A parameters
+
     :param z: array containing complex snrs for the detectors
     :param f_sig: sensitivity of detectors sigma * (F+, Fx)
+    :returns: the f-stat A parameters
     """
     m = zeros((2, 2))
     for f in f_sig:
@@ -179,20 +219,24 @@ def snr_f_to_a(z, f_sig):
     a = array([1.0, a_max[0].real, a_max[1].real, a_max[0].imag, a_max[1].imag])
     return a
 
+
 def a_f_to_snr(a, f_plus, f_cross):
     """
     Given the F-stat a parameters and f_plus, f_cross for a detector, calculate the SNR.
-    From the Harry-Fairhurst paper, the waveform is $h = A^{\mu} h_{\mu}$ where 
-    $h_1 = F_{+} h_{0}$, $h_2 = F_{\times} h_{0}$, $h_{3} = F_{+} h_{\pi/2}$, 
-    $h_{4} = F_{\times} h_{\pi/2}$ and $z = (s | h_{0}) + i (s | h_{\pi/2})$.  
-    Given the $A^{\mu}$, the expected SNR is:
-    $z = F_{+} A^{1} + F_{\times} A^{2} + i( F_{+} A^{3} + F_{\times} A^{4})$
+    From the Harry-Fairhurst paper, the waveform is :math:`h = A^{\mu} h_{\mu}` where 
+    :math:`h_1 = F_{+} h_{0}`; :math:`h_2 = F_x h_{0}`; :math:`h_{3} = F_{+} h_{\pi/2}`;
+    :math:`h_{4} = F_x h_{\pi/2}` and :math:`z = (s | h_{0}) + i (s | h_{\pi/2})`.
+    Given the :math:`A^{\mu}`, the expected SNR is:
+    :math:`z = F_{+} A^{1} + F_x A^{2} + i( F_{+} A^{3} + F_x A^{4})`
+
     :param a: F-stat parameters
     :param f_plus: Sensitivity to plus polarization
     :param f_cross: Sensitivity to cross polarization
+    :returns: expected SNR
     """
-    z = f_plus * (a[1] + 1j * a[3]) + f_cross * (a[2] + 1j * a[4]) 
+    z = f_plus * (a[1] + 1j * a[3]) + f_cross * (a[2] + 1j * a[4])
     return z
+
 
 # Calculate the dominant polarization F+, Fx and the angle between this and the original frame.
 
@@ -200,12 +244,12 @@ def dominant_polarization(f_sig):
     """
     Given the detector responses, compute the dominant polarization F+, Fx and the
     angle that we have to rotate through to get to D.P.
+
     :param f_sig: sensitivity of detectors: sigma * (F+, Fx)
     """
     m = zeros((2, 2))
     for f in f_sig:
         m += outer(f, f)
     f_cross, f_plus = sort(sqrt(linalg.eig(m)[0]))
-    chi = 1./4 * arctan2(2 * m[0,1], m[0,0] - m[1,1])
-    return f_plus, f_cross, chi 
-
+    chi = 1. / 4 * arctan2(2 * m[0, 1], m[0, 0] - m[1, 1])
+    return f_plus, f_cross, chi
