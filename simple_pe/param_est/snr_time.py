76882a3e8ec3a49c2f0681a7385d0b602329dd06
Stephen Fairhurst <stephen.fairhurst@ligo.org>
2022-05-30 12:19:27 +1000

    remove phenom

--- simple_pe/param_est/snr_time.py
+++ simple_pe/param_est/snr_time.py
@@ -1,9 +1,8 @@
 from __future__ import division
-import pylab
 import numpy
 import math
 from simple_pe.detectors import psd
-from simple_pe.waveform import stationary_phase, phenom
+from simple_pe.waveform import stationary_phase
 from scipy import integrate
 
 def calc_snr_vs_time(f, p, dt, m1,m2,toff=0):
