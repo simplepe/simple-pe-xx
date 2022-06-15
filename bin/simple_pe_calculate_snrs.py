8ce7c26883e864ec98160e6f3336dfa2cfdd39be
Stephen Fairhurst <stephen.fairhurst@ligo.org>
2022-06-15 10:31:34 +1000

    beginning of code to calculate event snrs

---
+++ bin/simple_pe_calculate_snrs.py
@@ -0,0 +1,22 @@
+
+
+injection_parameters = dict(
+    mass_1=32.3,
+    mass_2=10.1.,
+    a_1=0.4,
+    a_2=0.3,
+    tilt_1=0.5,
+    tilt_2=1.0,
+    phi_12=1.7,
+    phi_jl=0.3,
+    luminosity_distance=750.,
+    theta_jn=0.7,
+    psi=1.0,
+    phase=1.3,
+    geocent_time=1239082262.2 ,
+    ra=1.375,
+    dec=-1.2108)
+
+# Fixed arguments passed into the source model
+waveform_arguments = dict(waveform_approximant='IMRPhenomPv2',
+                          reference_frequency=50., minimum_frequency=20.)
