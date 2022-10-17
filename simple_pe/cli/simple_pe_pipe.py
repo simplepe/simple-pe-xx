241e3023ededf3e5b97f2da73e69b6324326171e
Charlie Hoy <charlie.hoy@port.ac.uk>
2022-10-17 13:22:09 +0100

    typo

--- simple_pe/cli/simple_pe_pipe.py
+++ simple_pe/cli/simple_pe_pipe.py
@@ -12,8 +12,8 @@ __authors__ = [
 def command_line():
     """Define the command line arguments for `simple_pe_pipe`
     """
-    from simple_pe_analysis import command_line as _analysis_command_line
-    from simple_pe_filter import command_line as _filter_command_line
+    from .simple_pe_analysis import command_line as _analysis_command_line
+    from .simple_pe_filter import command_line as _filter_command_line
     parser = ArgumentParser(
         parents=[_analysis_command_line(), _filter_command_line()],
         conflict_handler='resolve'
