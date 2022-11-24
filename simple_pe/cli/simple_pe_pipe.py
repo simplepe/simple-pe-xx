295eeea53a6373a68a6c741f32ff996bef004ab8
Charlie Hoy <hoyc1@cardiff.ac.uk>
2022-11-24 06:23:16 -0800

    epoch to t0

--- simple_pe/cli/simple_pe_pipe.py
+++ simple_pe/cli/simple_pe_pipe.py
@@ -357,14 +357,13 @@ class FilterNode(Node):
                 prepend = int(512 / ht.delta_t)
                 ht.append_zeros(prepend)
                 ht.prepend_zeros(prepend)
-                strain = TimeSeries(ht, epoch=ht.sample_times[0], dt=ht.delta_t)
+                strain = TimeSeries(ht, t0=ht.sample_times[0], dt=ht.delta_t)
                 strain.name = f"{ifo}:HWINJ_INJECTED"
                 strain.channel = f"{ifo}:HWINJ_INJECTED"
                 os.makedirs(f"{self.opts.outdir}/output", exist_ok=True)
                 filename = (
                     f"{self.opts.outdir}/output/{ifo}-INJECTION.gwf"
                 )
-                print(filename)
                 strain.write(filename)
                 _strain[f"{ifo}:HWINJ_INJECTED"] = filename
         return _strain
