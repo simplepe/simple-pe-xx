bd655ac144f27c00b5c7a2b4f7f3d9d73327eccb
Charlie Hoy <hoyc1@cardiff.ac.uk>
2022-11-24 05:08:16 -0800

    typo

--- simple_pe/cli/simple_pe_filter.py
+++ simple_pe/cli/simple_pe_filter.py
@@ -104,7 +104,7 @@ def command_line():
         "--metric_directions",
         help="Directions to calculate metric",
         nargs="+",
-        default=['chirp_mass', 'symmetric_mass_ratio', 'chi_align']
+        default=['chirp_mass', 'symmetric_mass_ratio', 'chi_align', 'chi_p2']
     )
     return parser
 
