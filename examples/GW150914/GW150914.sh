d68699e9ac7b250326fdf974dc704baea6900860
Charlie Hoy <hoyc1@cardiff.ac.uk>
2023-01-12 01:34:45 -0800

    remove echo statement

--- examples/GW150914/GW150914.sh
+++ examples/GW150914/GW150914.sh
@@ -13,7 +13,6 @@ if [ -z ${WVF} ]; then
     WVF="IMRPhenomXPHM"
 fi
 # then run `simple_pe_pipe`
-echo "simple_pe_pipe --strain H1:GWOSC:GW150914 L1:GWOSC:GW150914 --approximant ${WVF} --f_low 20 --f_high 2048 --accounting_group UNKNOWN --accounting_group_user albert.einstein --outdir ./outdir --trigger_parameters trigger_parameters.json --asd H1:GW150914_H1_asd.txt L1:GW150914_L1_asd.txt --metric_directions ${METRIC_DIRECTIONS} --precession_directions ${PREC_DIRECTIONS}"
 simple_pe_pipe --strain H1:GWOSC:GW150914 L1:GWOSC:GW150914 --approximant ${WVF} --f_low 20 --f_high 2048 --accounting_group UNKNOWN --accounting_group_user albert.einstein --outdir ./outdir --trigger_parameters trigger_parameters.json --asd H1:GW150914_H1_asd.txt L1:GW150914_L1_asd.txt --metric_directions ${METRIC_DIRECTIONS} --precession_directions ${PREC_DIRECTIONS}
 # then run the output
 bash ./outdir/submit/bash_simple_pe.sh
