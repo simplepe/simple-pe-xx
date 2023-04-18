b4b1425c7fa50fd275efa05c3eabce8d044be4db
Stephen Fairhurst <stephen.fairhurst@ligo.org>
2023-04-18 17:15:57 +0100

    update docs

--- simple_pe/detectors/dets.py
+++ simple_pe/detectors/dets.py
@@ -9,21 +9,22 @@ from pesummary.gw.conversions.mass import mchirp_from_m1_m2
 class Det(detector.Detector):
     """
     class to hold the details of a detector
+
+    Parameters
+    ----------
+    detector_name: 2 character string for detector
+    det_range: the BNS range of the detector
+    f_mean: float, mean frequency
+    f_band: float, frequency bandwidth
+    found_thresh: threshold for declaring an event found
+    loc_thresh: threshold for declaring an event localized
+    duty_cycle: fraction of time the detector is operational
+    bns_range: is the given range for BNS (if yes, then rescale SNR with mchirp^5/6)
     """
 
     def __init__(self, detector_name, det_range, f_mean, f_band,
                  found_thresh=5.0, loc_thresh=4.0, duty_cycle=1.0, bns_range=True):
-        """
-        Initialize
-        :param detector_name: 2 character string for detector
-        :param det_range: the BNS range of the detector
-        :param f_mean: float, mean frequency
-        :param f_band: float, frequency bandwidth
-        :param found_thresh: threshold for declaring an event found
-        :param loc_thresh: threshold for declaring an event localized
-        :param duty_cycle: fraction of time the detector is operational
-        :param bns_range: is the given range for BNS (if yes, then rescale SNR with mchirp^5/6)
-        """
+
         super().__init__(detector_name)
         self.det_range = det_range
         self.sigma = 2.26 * det_range * 8  # this gives the SNR at 1 Mpc
@@ -44,7 +45,9 @@ class Det(detector.Detector):
         """
         Calculate the sensitivity of the detector to an event
 
-        :param event: object, containing ra, dec, psi, gmst
+        Parameters
+        ----------
+        event: object, containing ra, dec, psi, gmst
         """
         self.f_plus, self.f_cross = self.antenna_pattern(event.ra, event.dec, event.psi,
                                                          event.gps)
@@ -53,7 +56,9 @@ class Det(detector.Detector):
         """
         Calculate the sensitivity of the detector to an event, in its mirror sky location
 
-        :param event: object, containing mirror_ra, mirror_dec, psi, gmst
+        Parameters
+        ----------
+        event: object, containing mirror_ra, mirror_dec, psi, gmst
         """
         self.mirror_f_plus, self.mirror_f_cross = self.antenna_pattern(event.mirror_ra,
                                                                        event.mirror_dec,
@@ -63,8 +68,13 @@ class Det(detector.Detector):
         """
         Calculate the expected SNR of the event in the detector
 
-        :param event: object, containing ra, dec, psi, gmst, phi, cosi
-        :returns: the complex SNR for the signal
+        Parameters
+        ----------
+        event: object, containing ra, dec, psi, gmst, phi, cosi
+
+        Returns
+        -------
+        the complex SNR for the signal
         """
         self.calculate_sensitivity(event)
         if self.bns_range:
@@ -78,7 +88,12 @@ class Det(detector.Detector):
         """
         Method to return the sensitivity of the detector
 
-        :param mirror: boolean, is this the mirror position
+        Parameters
+        ---------
+        mirror: boolean, is this the mirror position
+
+        Returns
+        -------
         :returns: length 2 array: sigma * (F_plus, F_cross)
         """
         if mirror:
