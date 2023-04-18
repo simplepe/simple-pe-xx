daedbd68ca4c0af2a786d457914c93ef76da1f34
Stephen Fairhurst <stephen.fairhurst@ligo.org>
2023-04-18 17:26:32 +0100

    updating network docs

--- simple_pe/detectors/network.py
+++ simple_pe/detectors/network.py
@@ -10,27 +10,31 @@ from simple_pe.detectors import dets
 class Network(object):
     """
     class to hold the details of the network.
+
+    Parameters
+    ----------
+    threshold: detection threshold for the network
     """
 
     def __init__(self, threshold=12.0):
-        """
-
-        :param threshold: detection threshold for the network
-        """
         self.threshold = threshold
         self.ifos = []
 
     def add_ifo(self, ifo, det_range, f_mean, f_band,
                 found_thresh=5.0, loc_thresh=4.0, duty_cycle=1.0, bns_range=True):
         """
-        :param ifo: name of ifo
-        :param det_range: the BNS range of the detector
-        :param f_mean: float, mean frequency
-        :param f_band: float, frequency bandwidth
-        :param found_thresh: threshold for declaring an event found
-        :param loc_thresh: threshold for declaring an event localized
-        :param duty_cycle: fraction of time the detector is operational
-        :param bns_range: is the given range for BNS (if yes, then rescale SNR with mchirp^5/6)
+        Add an ifo to a network
+
+        Parameters
+        ----------
+        ifo: name of ifo
+        det_range: the BNS range of the detector
+        f_mean: float, mean frequency
+        f_band: float, frequency bandwidth
+        found_thresh: threshold for declaring an event found
+        loc_thresh: threshold for declaring an event localized
+        duty_cycle: fraction of time the detector is operational
+        bns_range: is the given range for BNS (if yes, then rescale SNR with chirp_mass^5/6)
         """
         d = dets.Det(ifo, det_range, f_mean, f_band,
                      found_thresh, loc_thresh, duty_cycle, bns_range)
@@ -43,10 +47,12 @@ class Network(object):
         set the details of the detectors based on the given configuration.
         data is stored in the detectors module
 
-        :param configuration: name of configuration
-        :param found_thresh: threshold for single ifo detection
-        :param loc_thresh: threshold for single ifo localization
-        :param duty_cycle: fraction of time detectors are operational
+        Parameters
+        ----------
+        configuration: name of configuration
+        found_thresh: threshold for single ifo detection
+        loc_thresh: threshold for single ifo localization
+        duty_cycle: fraction of time detectors are operational
         """
         ranges = detectors.range_8(configuration)
         ifos = ranges.keys()
@@ -59,7 +65,12 @@ class Network(object):
         """
         get the relevant data for each detector and return it as an array
 
-        :param data: name of data to return from a detector
-        :return array containing requested data
+        Parameters
+        ----------
+        data: name of data to return from a detector
+
+        Returns
+        -------
+        data_array: array containing requested data
         """
         return np.array([getattr(getattr(self, i), data) for i in self.ifos])
