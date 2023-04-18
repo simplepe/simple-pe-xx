3b1038d13b589ea7c178bb26d5a0da1d488c7f55
Charlie Hoy <hoyc1@cardiff.ac.uk>
2023-04-18 05:11:33 -0700

    remove unused filed

--- examples/GW150914/GW150914.sh
+++
@@ -1,4 +0,0 @@
-# shell script to analyse GW150914
-simple_pe_pipe config.ini
-# then run the output
-bash ./outdir/submit/bash_simple_pe.sh
--- examples/zero-noise/zero-noise.sh
+++
@@ -1,4 +0,0 @@
-# shell script to analyse a zero-noise injection
-simple_pe_pipe config.ini
-# then run the output
-bash ./outdir/submit/bash_simple_pe.sh
