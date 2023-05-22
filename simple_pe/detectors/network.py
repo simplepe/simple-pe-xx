b31286923a45682f0d96debe77e38a462a63b5f6
Stephen Fairhurst <stephen.fairhurst@ligo.org>
2023-05-22 18:48:39 +0100

    add function to generate network from psds

--- simple_pe/detectors/network.py
+++ simple_pe/detectors/network.py
@@ -1,6 +1,6 @@
 import numpy as np
-from simple_pe.detectors import detectors
-from simple_pe.detectors import dets
+from simple_pe import detectors
+from simple_pe.detectors import Det
 
 ##################################################################
 # Class to store network information
@@ -20,8 +20,9 @@ class Network(object):
         self.threshold = threshold
         self.ifos = []
 
-    def add_ifo(self, ifo, det_range, f_mean, f_band,
-                found_thresh=5.0, loc_thresh=4.0, duty_cycle=1.0, bns_range=True):
+    def add_ifo(self, ifo, horizon, f_mean, f_band,
+                found_thresh=5.0, loc_thresh=4.0, duty_cycle=1.0,
+                bns_range=True):
         """
         Add an ifo to a network
 
@@ -36,8 +37,8 @@ class Network(object):
         duty_cycle: fraction of time the detector is operational
         bns_range: is the given range for BNS (if yes, then rescale SNR with chirp_mass^5/6)
         """
-        d = dets.Det(ifo, det_range, f_mean, f_band,
-                     found_thresh, loc_thresh, duty_cycle, bns_range)
+        d = Det(ifo, horizon, f_mean, f_band,
+                found_thresh, loc_thresh, duty_cycle, bns_range)
         setattr(self, ifo, d)
         self.ifos.append(ifo)
 
@@ -59,7 +60,39 @@ class Network(object):
         fmeans = detectors.fmean(configuration)
         fbands = detectors.bandwidth(configuration)
         for ifo in ifos:
-            self.add_ifo(ifo, ranges[ifo], fmeans[ifo], fbands[ifo], found_thresh, loc_thresh, duty_cycle)
+            self.add_ifo(ifo, 2.26 * ranges[ifo], fmeans[ifo], fbands[ifo],
+                         found_thresh, loc_thresh, duty_cycle)
+
+    def generate_network_from_psds(self, ifos, psds, f_lows,
+                                   approximant='IMRPhenomD',
+                                   found_thresh=5.0, loc_thresh=4.0,
+                                   duty_cycle=1.0, bns_range=True):
+        """
+        Generate a network from a list of ifos, with associated PSDs
+         and f_lows
+
+        Parameters
+        ----------
+        ifos: list of ifos
+        psds: dictionary of psds associated with the ifos
+        f_lows: dictionary of low frequency cutoffs for each ifo
+        approximant: approximant to use in waveform generation
+        found_thresh: threshold for declaring an event found
+        loc_thresh: threshold for declaring an event localized
+        duty_cycle: fraction of time the detector is operational
+        bns_range: is the given range for BNS
+            (if yes, then rescale SNR with chirp_mass^5/6)
+        """
+        for ifo in ifos:
+            horizon, f_mean, f_band = \
+                detectors.calc_reach_bandwidth(mass1=1.4, mass2=1.4, spin=0,
+                                               approx=approximant,
+                                               power_spec=psds[ifo],
+                                               fmin=f_lows[ifo],
+                                               thresh=8)
+            self.add_ifo(ifo, horizon, f_mean, f_band,
+                         found_thresh, loc_thresh, duty_cycle,
+                         bns_range)
 
     def get_data(self, data):
         """
