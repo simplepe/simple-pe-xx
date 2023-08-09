1dd3a85bd7c7d22c32836df8a5b5d7ee146fc2ea
Stephen Fairhurst <stephen.fairhurst@ligo.org>
2023-08-09 20:23:14 +0100

    updating to take command line arguments

--- simple_pe/cli/localize/plot_localization_ellipses.py
+++ simple_pe/cli/localize/plot_localization_ellipses.py
@@ -1,12 +1,17 @@
+#! /usr/bin/env python
+
 import numpy as np
 import pylab as plt
+import os
+from argparse import ArgumentParser
 
-from simple_pe.localization import event
-from simple_pe.detectors import detectors, network
-
+from simple_pe import localization
+from simple_pe import detectors
 import cartopy.crs as ccrs
 
-'''For plotting the L shaped interferometer marker have to manually draw them as in this cell:
+'''
+For plotting the L shaped interferometer marker have to manually draw them 
+as in this cell:
 '''
 from matplotlib.path import Path
 
@@ -21,133 +26,184 @@ codes = [Path.MOVETO,
          ]
 path = Path(verts, codes)
 
-net_state = "design"
-method = 'time'
-found_thresh = 5.0
-net_thresh = 12.0
-loc_thresh = 4.0
-duty_cycle = 1.0
 
-BNS_min_chirp_mass = 1.4 * 2 ** (-1. / 5)
+def command_line():
+    """Define the command line arguments for `simple_pe_localization_ellipses`
+    """
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--seed",
+        help="random seed to set for reproducibility",
+        default=123456789,
+        type=int
+    )
+    parser.add_argument(
+        "--net-state",
+        help="the network state (one of those defined in detectors.py)",
+        default="design",
+        type=str,
+    )
+    parser.add_argument(
+        "--method",
+        help=(
+            "localization method."
+            "One of ('time', 'coh', 'left', 'right', 'marg')"
+        ),
+        default="time",
+        type=str,
+    )
+    parser.add_argument(
+        "--found-thresh",
+        help="Threshold at which signal is observed in single detector",
+        default=5.0,
+        type=float,
+    )
+    parser.add_argument(
+        "--net-thresh",
+        help="Network threshold for detection",
+        default=12.0,
+        type=float,
+    )
+    parser.add_argument(
+        "--loc-thresh",
+        help="Threshold for a detector to contribute to "
+             "localization",
+        default=4.0,
+        type=float,
+    )
+    parser.add_argument(
+        "--duty-cycle",
+        help="Duty cycle for each detector (assumed equal and independent "
+             "for all detectors)",
+        default=1.,
+        type=float,
+    )
+    parser.add_argument(
+        "--npoints",
+        help="Number of points in right-ascension and declination to use",
+        default=16,
+        type=int,
+    )
+    parser.add_argument(
+        "--distance",
+        help="Distance at which to simulate signals",
+        default=200.,
+        type=float,
+    )
+    parser.add_argument(
+        "--outdir",
+        help="Directory to store the output",
+        default="./",
+    )
 
-savefig = True
-savetxt = False
+    return parser
 
-# set up trials
-dra, ddec = np.pi / 8.0, np.pi / 16.0
-[ra, dec] = np.mgrid[-np.pi + dra:np.pi:dra, -np.pi / 2 + ddec:np.pi / 2:ddec]
 
-ra = ra.flatten()
-dec = dec.flatten()
-Dco = 200.
-psi = 0.
-cosi = 1.
-gps = float(999995380)
+def main(args=None):
+    """Main interface for `simple_pe_analysis`
+    """
+    parser = command_line()
+    opts, _ = parser.parse_known_args(args=args)
+    np.random.seed(opts.seed)
 
-ntrials = len(ra)
+    if not os.path.isdir(opts.outdir):
+        os.mkdir(opts.outdir)
 
-params = {}
-params['distance'] = Dco
-params['gps'] = gps
-params['coa-phase'] = 0.
-params['polarization'] = psi
-params['inclination'] = np.arccos(cosi)
-params['mass1'] = 1.4
-params['mass2'] = 1.4
-params['RAdeg'] = 0.
-params['DEdeg'] = 0.
+    # set up trials
+    dra, ddec = 2 * np.pi / opts.npoints, np.pi / opts.npoints
+    [ra, dec] = np.mgrid[-np.pi + dra:np.pi:dra,
+                -np.pi / 2 + ddec:np.pi / 2:ddec]
+    ra = ra.flatten()
+    dec = dec.flatten()
 
-num_found, num_loc, all_lists = {}, {}, {}
-Ms = {}
+    params = {'distance': opts.distance,
+              'gps': float(999995380),
+              'coa-phase': 0.,
+              'polarization': 0.,
+              'inclination': 0.,
+              'mass1': 1.4,
+              'mass2': 1.4,
+              'RAdeg': 0.,
+              'DEdeg': 0.}
 
-n = network.Network()
-n.set_configuration(net_state, found_thresh, loc_thresh, duty_cycle)
+    n = detectors.Network()
+    n.set_configuration(opts.net_state,
+                        opts.found_thresh,
+                        opts.loc_thresh,
+                        opts.duty_cycle)
 
-nf, nl = 0, 0
-all_list = []
-for trial, r in enumerate(ra):
-    params['RAdeg'] = np.degrees(r)
-    params['DEdeg'] = np.degrees(dec[trial])
-    ev = event.Event.from_params(params=params)
-    ev.add_network(n)
-    all_list.append(ev)
-    if ev.detected:
-        nf += 1
-        if ev.localized >= 3:
-            nl += 1
-            ev.localize_all()
+    nf, nl = 0, 0
+    all_list = []
+    for trial, (r,d) in enumerate(zip(ra, dec)):
+        params['RAdeg'] = np.degrees(r)
+        params['DEdeg'] = np.degrees(d)
+        ev = localization.Event.from_params(params=params)
+        ev.add_network(n)
+        all_list.append(ev)
+        if ev.detected:
+            nf += 1
+            if ev.localized >= 3:
+                nl += 1
+                ev.localize_all()
 
-# store in network dictionaries
-all_list
-num_found = nf
-num_loc = nl
+    # Plot the localization matrix for each point
+    plt.figure(figsize=[20, 20])
+    ax = plt.axes(projection=ccrs.Mollweide())
+    data_crs = ccrs.PlateCarree()
+    ax.coastlines()
 
-# check all were localized
-print('%s events were not localized by %s' % (ntrials - num_loc, net_state))
+    for i, ev in enumerate(all_list):
+        if ev.detected and ev.localized >= 3:
+            phi, theta = ev.localization['time'].make_ellipse()
+            ax.plot(np.degrees((phi - np.pi) % (2 * np.pi) + np.pi),
+                    np.degrees(theta), 'g', transform=data_crs)
+        else:
+            ax.plot(np.degrees(ev.ra - ev.gmst), np.degrees(ev.dec), 'rx',
+                    markersize=6, markeredgewidth=1,
+                    transform=data_crs)
 
-patches = []
-detect = []
-for ev in all_list:
-    detect.append(ev.detected)
-    try:
-        patches.append(ev.patches[method])
-    except:
-        KeyError
-patches = np.array(patches)
-print("%s: Fraction localized to one patch = %.3f" %
-      (net_state, (1. * sum(patches == 1) / len(patches))))
-
-# Get the localization matrix for each point
-
-plt.figure(figsize=[20, 20])
-ax = plt.axes(projection=ccrs.Mollweide())
-data_crs = ccrs.PlateCarree()
-ax.coastlines()
-
-for i, ev in enumerate(all_list):
-    if ev.detected and ev.localized >= 3:
-        phi, theta = ev.localization['time'].make_ellipse()
-        ax.plot(np.degrees(phi), np.degrees(theta), 'g', transform=data_crs)
-    else:
-        ra = (ev.ra - ev.gmst) % (2 * np.pi)
-        ax.plot(np.degrees(ev.ra - ev.gmst), np.degrees(ev.dec), 'rx', markersize=6, markeredgewidth=1,
+    for ifo in ev.ifos:
+        i = ev.__getattribute__(ifo)
+        phi, theta = detectors.phitheta(i.location / np.linalg.norm(i.location))
+        ax.plot(np.degrees(phi), np.degrees(theta), marker=path, markersize=25,
+                markerfacecolor='w', markeredgecolor='k', markeredgewidth=4,
                 transform=data_crs)
 
-for ifo in ev.ifos:
-    i = ev.__getattribute__(ifo)
-    phi, theta = detectors.phitheta(i.location / np.linalg.norm(i.location))
-    ax.plot(np.degrees(phi), np.degrees(theta), marker=path, markersize=25,
-            markerfacecolor='w', markeredgecolor='k', markeredgewidth=4,
-            transform=data_crs)
+    plt.title(opts.net_state, fontsize=24)
+    plt.savefig('%s_sky_ellipses.png' % opts.net_state)
 
-plt.title(net_state, fontsize=24)
-if savefig:
-    plt.savefig('%s_sky_ellipses.png' % net_state)
+    plt.figure(figsize=[20, 20])
+    ax = plt.axes(projection=ccrs.Mollweide())
+    data_crs = ccrs.PlateCarree()
+    ax.coastlines()
 
-plt.figure(figsize=[20, 20])
-ax = plt.axes(projection=ccrs.Mollweide())
-data_crs = ccrs.PlateCarree()
-ax.coastlines()
+    for i, ev in enumerate(all_list):
+        if not ev.detected:
+            ax.plot(np.degrees(ev.ra - ev.gmst), np.degrees(ev.dec), '#ff0325',
+                    marker='x',
+                    markersize=6, markeredgewidth=1, transform=data_crs)
+        elif ev.localized < 3:
+            ax.plot(np.degrees(ev.ra - ev.gmst), np.degrees(ev.dec), '#0346ff',
+                    marker='x',
+                    markersize=6, markeredgewidth=1, transform=data_crs)
+        else:
+            phi, theta = ev.localization['time'].make_ellipse()
+            ax.plot(np.degrees((phi - np.pi) % (2 * np.pi) + np.pi),
+                    np.degrees(theta), 'g', transform=data_crs)
 
-for i, ev in enumerate(all_list):
-    if not ev.detected:
-        ra = (ev.ra - ev.gmst) % (2 * np.pi)
-        ax.plot(np.degrees(ev.ra - ev.gmst), np.degrees(ev.dec), '#ff0325', marker='x',
-                markersize=6, markeredgewidth=1, transform=data_crs)
-    elif ev.localized < 3:
-        ra = (ev.ra - ev.gmst) % (2 * np.pi)
-        ax.plot(np.degrees(ev.ra - ev.gmst), np.degrees(ev.dec), '#0346ff', marker='x',
-                markersize=6, markeredgewidth=1, transform=data_crs)
-    else:
-        phi, theta = ev.localization['time'].make_ellipse()
-        ax.plot(np.degrees(phi), np.degrees(theta), 'g', transform=data_crs)
+    for ifo in ev.ifos:
+        i = ev.__getattribute__(ifo)
+        phi, theta = detectors.phitheta(i.location / np.linalg.norm(i.location))
+        ax.plot(np.degrees(phi), np.degrees(theta), marker=path, markersize=25,
+                markerfacecolor='w', markeredgecolor='k', markeredgewidth=4,
+                transform=data_crs)
 
-for ifo in ev.ifos:
-    i = ev.__getattribute__(ifo)
-    phi, theta = detectors.phitheta(i.location / np.linalg.norm(i.location))
-    ax.plot(np.degrees(phi), np.degrees(theta), marker=path, markersize=25,
-            markerfacecolor='w', markeredgecolor='k', markeredgewidth=4, transform=data_crs)
+    plt.title(opts.net_state, fontsize=24)
+    plt.savefig('%s_sky_ellipses_found.png' % opts.net_state)
 
-plt.title(net_state, fontsize=24)
-if savefig:
-    plt.savefig('%s_sky_ellipses_found.png' % net_state)
+
+if __name__ == "__main__":
+    main()
+
+
+
