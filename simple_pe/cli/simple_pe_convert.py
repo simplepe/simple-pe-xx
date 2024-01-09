43c34f7d81937d26d22e9a707662f92ad93c9936
Charlie Hoy <hoyc1@cardiff.ac.uk>
2024-01-09 01:13:26 -0800

    separate conversion

---
+++ simple_pe/cli/simple_pe_convert.py
@@ -0,0 +1,47 @@
+from simple_pe.param_est import pe
+from pesummary.io import read
+from pesummary.core.command_line import CheckFilesExistAction
+import argparse
+
+__authors__ = [
+    "Charlie Hoy <charlie.hoy@ligo.org>",
+]
+
+
+def command_line():
+    """Define the command line arguments for `simple_pe_convert`
+    """
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--posterior",
+        help=(
+            "File containing posterior samples. Must be compatible with "
+            "pesummary.io.read"
+        ),
+        required=True,
+        action=CheckFilesExistAction
+    )
+    parser.add_argument(
+        "--outdir",
+        help="Directory to store the output",
+        default="./",
+    )
+    return parser
+
+
+def main(args=None):
+    """Main interface for `simple_pe_convert`
+    """
+    parser = command_line()
+    opts, _ = parser.parse_known_args(args=args)
+    samples = pe.SimplePESamples(read(opts.posterior).samples_dict)
+    samples.generate_all_posterior_samples(
+        function=pe._add_chi_align
+    )
+    samples.generate_all_posterior_samples(
+        function=pe._component_spins_from_chi_align
+    )
+    samples.write(
+        file_format="dat", outdir=opts.outdir,
+        filename="converted_posterior_samples.dat"
+    )
